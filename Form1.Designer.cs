3ca120b02ba69c95679f45479c15f347348e0af3
Kat <kat@kattschan.co.uk>
2024-12-14 12:53:23 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinFormsB2PShell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.greeting = new System.Windows.Forms.Label();
+            this.launcherButton = new System.Windows.Forms.Button();
+            this.logOff = new System.Windows.Forms.Button();
+            this.lockButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shutDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // greeting
+            // 
+            this.greeting.AutoSize = true;
+            this.greeting.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greeting.Location = new System.Drawing.Point(12, 9);
+            this.greeting.Name = "greeting";
+            this.greeting.Size = new System.Drawing.Size(221, 37);
+            this.greeting.TabIndex = 0;
+            this.greeting.Text = "Good Morning";
+            // 
+            // launcherButton
+            // 
+            this.launcherButton.AutoSize = true;
+            this.launcherButton.Location = new System.Drawing.Point(19, 49);
+            this.launcherButton.Name = "launcherButton";
+            this.launcherButton.Padding = new System.Windows.Forms.Padding(1);
+            this.launcherButton.Size = new System.Drawing.Size(75, 25);
+            this.launcherButton.TabIndex = 1;
+            this.launcherButton.Text = "button1";
+            this.launcherButton.UseVisualStyleBackColor = true;
+            this.launcherButton.Click += new System.EventHandler(this.launcherButton_Click);
+            // 
+            // logOff
+            // 
+            this.logOff.Location = new System.Drawing.Point(18, 98);
+            this.logOff.Name = "logOff";
+            this.logOff.Padding = new System.Windows.Forms.Padding(1);
+            this.logOff.Size = new System.Drawing.Size(75, 23);
+            this.logOff.TabIndex = 2;
+            this.logOff.Text = "Log Off";
+            this.logOff.UseVisualStyleBackColor = true;
+            this.logOff.Click += new System.EventHandler(this.logOff_Click);
+            // 
+            // lockButton
+            // 
+            this.lockButton.Location = new System.Drawing.Point(99, 98);
+            this.lockButton.Name = "lockButton";
+            this.lockButton.Padding = new System.Windows.Forms.Padding(1);
+            this.lockButton.Size = new System.Drawing.Size(75, 23);
+            this.lockButton.TabIndex = 3;
+            this.lockButton.Text = "Lock";
+            this.lockButton.UseVisualStyleBackColor = true;
+            this.lockButton.Click += new System.EventHandler(this.lockButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Power Options";
+            // 
+            // shutDown
+            // 
+            this.shutDown.Location = new System.Drawing.Point(180, 98);
+            this.shutDown.Name = "shutDown";
+            this.shutDown.Padding = new System.Windows.Forms.Padding(1);
+            this.shutDown.Size = new System.Drawing.Size(75, 23);
+            this.shutDown.TabIndex = 5;
+            this.shutDown.Text = "Shut Down";
+            this.shutDown.UseVisualStyleBackColor = true;
+            this.shutDown.Click += new System.EventHandler(this.shutDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shutDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lockButton);
+            this.Controls.Add(this.logOff);
+            this.Controls.Add(this.launcherButton);
+            this.Controls.Add(this.greeting);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label greeting;
+        private System.Windows.Forms.Button launcherButton;
+        private System.Windows.Forms.Button logOff;
+        private System.Windows.Forms.Button lockButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button shutDown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsB2PShell
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool LockWorkStation();
+
+        private const int SW_MAXIMIZE = 3;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Process.Start("C:\\Program Files\\Parsec\\parsecd.exe");
+            CheckParsecdStatus();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private async void CheckParsecdStatus()
+        {
+            while (true)
+            {
+                if (IsParsecdRunning())
+                {
+                    launcherButton.Enabled = false;
+                    launcherButton.Text = "Parsec is running";
+                }
+                else
+                {
+                    launcherButton.Enabled = true;
+                    launcherButton.Text = "Relaunch";
+                }
+                await Task.Delay(1000); // Check every second
+            }
+        }
+
+        private bool IsParsecdRunning()
+        {
+            return Process.GetProcessesByName("parsecd").Length > 0;
+        }
+
+        private void launcherButton_Click(object sender, EventArgs e)
+        {
+            if (!IsParsecdRunning())
+            {
+                Process.Start("C:\\Program Files\\Parsec\\parsecd.exe");
+            }
+        }
+
+        private void logOff_Click(object sender, EventArgs e)
+        {
+            // Show a confirmation dialog
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to log off?",
+                "Confirm Log Off",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            // Check the user's response
+            if (result == DialogResult.Yes)
+            {
+                // Log off the user
+                ExitWindowsEx(0, 0); // EWX_LOGOFF = 0
+            }
+            else
+            {
+                // Continue execution (do nothing)
+            }
+        }
+
+        private void lockButton_Click(object sender, EventArgs e)
+        {
+            LockWorkStation();
+        }
+
+        private void shutDown_Click(object sender, EventArgs e)
+        {
+            var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+    }
+}
